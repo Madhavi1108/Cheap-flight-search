b0d53cbdcf092a721bccb968ef5c0a043ac0a1c8
Madhavi <madhavi09307@gmail.com>
2025-02-05 20:16:09 +0530

    Update flight_data.py

--- flight_data.py
+++ flight_data.py
@@ -1,15 +1,6 @@
 class FlightData:
 
     def __init__(self, price, origin_airport, destination_airport, out_date, return_date):
-        """
-        Constructor for initializing a new flight data instance with specific travel details.
-        Parameters:
-        - price: The cost of the flight.
-        - origin_airport: The IATA code for the flight's origin airport.
-        - destination_airport: The IATA code for the flight's destination airport.
-        - out_date: The departure date for the flight.
-        - return_date: The return date for the flight.
-        """
         self.price = price
         self.origin_airport = origin_airport
         self.destination_airport = destination_airport
